16b1d2f8ed417bba0670eb7dbebe8bfd8a984def
Tim Martin <tim@timmart.in>
2024-07-02 14:36:09 -0500

    crazy stuff to get collapsable nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,47 @@
 <script lang="ts">
+	let navCollapsed = true;
 </script>
 
 <div class="container">
 	<header>
 		<nav>
-			<a href="/" aria-label="Home" id="foo">
-				<svg viewBox="-34.5 0 122 100" xmlns="http://www.w3.org/2000/svg" aria-label="Home Icon">
-					<path
-						d="M0 0v100-40a10 10 180 1 1 30 0v18a10 10 180 1 0 30 0V38v40a10 10 180 1 0 30 0V38"
-						fill="none"
-						stroke-width="15"
-						transform="skewX(-15)"
-					/>
-				</svg></a
-			>
-			<ol>
+			<ol class:collapsed={navCollapsed}>
+				<li class="nav-buttons">
+					<button
+						class="hamburger"
+						on:click={() => (navCollapsed = !navCollapsed)}
+						aria-label="Toggle Navigation"
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="100%" viewBox="0 0 24 24"
+							><path
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M5 17h14M5 12h14M5 7h14"
+							/></svg
+						>
+					</button>
+					<!-- <a href="/" aria-label="Home"> -->
+					<svg
+						class="logo"
+						viewBox="-34.5 0 122 100"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-label="Home Icon"
+					>
+						<path
+							d="M0 0v100-40a10 10 180 1 1 30 0v18a10 10 180 1 0 30 0V38v40a10 10 180 1 0 30 0V38"
+							fill="none"
+							stroke-width="15"
+							transform="skewX(-15)"
+						/>
+					</svg>
+					<!-- </a> -->
+				</li>
 				<li><a href="/">Home</a></li>
 				<li><a href="https://discord.gg/fYS3BTXJdr">Discord</a></li>
 				<li><a href="/internet">My Optimum Internet</a></li>
+				<li><a href="https://trashthisweek.com/">Trash This Week</a></li>
 			</ol>
 		</nav>
 	</header>
@@ -36,29 +60,68 @@
 		max-width: 800px;
 	}
 
-	nav {
+	nav ol {
 		margin-bottom: 1.5rem;
 		margin-top: 1rem;
 		display: flex;
-		align-items: flex-start;
+		align-items: baseline;
 		gap: 1rem;
+		flex-direction: column;
+		list-style-type: none;
+		display: flex;
+		gap: 1rem;
+		padding-inline-start: 0;
+	}
+
+	nav ol .hamburger {
+		display: unset;
+	}
+
+	nav ol.collapsed li:not(.hamburger, .nav-buttons) {
+		display: none;
+	}
+
+	.nav-buttons {
+		display: flex;
+		align-items: center;
+        gap: 1rem;
+	}
+
+	@media (min-width: 600px) {
+		nav ol {
+			flex-direction: row;
+		}
+
+		nav ol.collapsed li:not(.nav-buttons) {
+			display: unset;
+            align-self: flex-end;
+		}
+
+		nav ol li .hamburger {
+			display: none;
+		}
 	}
 
 	svg {
 		width: 3rem;
 		min-width: 3rem;
+	}
+
+	svg.logo {
 		stroke: var(--up-color);
 	}
 
-	a:hover svg {
+    .hamburger {
+        padding: 0;
+    }
+
+	.hamburger svg {
 		stroke: var(--text-color);
+		border: 1px solid var(--text-color);
+		border-radius: 0.5rem;
 	}
 
-	nav ol {
-		list-style-type: none;
-		display: flex;
-		gap: 1rem;
-		padding-inline-start: 0;
-		align-items: center;
+	.hamburger:hover svg {
+		stroke: var(--up-color);
 	}
 </style>
