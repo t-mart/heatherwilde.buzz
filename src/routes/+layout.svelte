8ff7911af590a29c4b0b8684e03164f734893151
Tim Martin <tim@timmart.in>
2024-07-09 00:07:03 -0500

    link logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,19 +22,21 @@
 							/></svg
 						>
 					</button>
-					<svg
-						class="logo"
-						viewBox="-34.5 0 122 100"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-label="Home Icon"
+					<a href="/">
+						<svg
+							class="logo"
+							viewBox="-34.5 0 122 100"
+							xmlns="http://www.w3.org/2000/svg"
+							aria-label="Home Icon"
+						>
+							<path
+								d="M0 0v100-40a10 10 180 1 1 30 0v18a10 10 180 1 0 30 0V38v40a10 10 180 1 0 30 0V38"
+								fill="none"
+								stroke-width="15"
+								transform="skewX(-15)"
+							/>
+						</svg></a
 					>
-						<path
-							d="M0 0v100-40a10 10 180 1 1 30 0v18a10 10 180 1 0 30 0V38v40a10 10 180 1 0 30 0V38"
-							fill="none"
-							stroke-width="15"
-							transform="skewX(-15)"
-						/>
-					</svg>
 				</li>
 				<li><a href="/">Home</a></li>
 				<li><a href="https://discord.gg/fYS3BTXJdr">Discord</a></li>
@@ -112,9 +114,9 @@
 		}
 	}
 
-    nav {
-        margin-bottom: 2rem;
-    }
+	nav {
+		margin-bottom: 2rem;
+	}
 
 	svg {
 		width: 3rem;
