cebc4239b8065391f46bb7fc8dcf9cd31d7ed8ba
Tim Martin <tim@timmart.in>
2025-06-05 23:35:55 -0500

    fresh next

--- src/lib/timeframeStore.ts
+++
@@ -1,4 +0,0 @@
-import { Timeframe } from '$lib';
-import { type Writable, writable } from 'svelte/store';
-
-export const timeframeStore: Writable<Timeframe> = writable(Timeframe.DAY);
--- src/routes/optimum/+page.ts
+++
@@ -1,3 +0,0 @@
-// we need this route to run in the browser because the viewport width store
-// uses the browser's ResizeObserver
-export const ssr = false;
